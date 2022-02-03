ba7d645d094c2071e66904ae1d82b9efb5f5f1dc
luis <hilario.huaman.6@gmail.com>
2022-02-03 15:08:38 -0500

    casi terminado de design

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -4,6 +4,14 @@ class DesignsController < ApplicationController
     @designs = Design.where(portfolio_id: params[:portfolio_id])
     @portfolio = Portfolio.find(params[:portfolio_id])
     @design = Design.new
+
+    if params[:query].present?
+      @designs = @designs.where('title ILIKE ?', "%#{params[:query]}%")
+    end
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'designs/list', locals: { designs: @designs }, formats: [:html] }
+    end
   end
 
   def new
@@ -34,19 +42,27 @@ class DesignsController < ApplicationController
   end
 
   def update
-    # @portfolio = Portfolio.find(params[:portfolio_id])
     @design = Design.find(params[:id])
-    if @design.update(params.require(:design).permit(:title, :description))
-      redirect_to portfolio_designs_path(@design.portfolio_id)
-    else
-      render :edit
+    # if @design.update(params.require(:design).permit(:title, :description))
+      # redirect_to portfolio_designs_path(@design.portfolio_id)
+    # else
+      # render :edit
+    # end
+    @design.update(params.require(:design).permit(:title, :description))
+    respond_to do |format|
+      format.html { redirect_to portfolio_designs_path(@design.portfolio_id) }
+      format.text { render partial: 'designs/design', locals: { design: @design }, formats: [:html] }
     end
   end
 
   def destroy
     @design = Design.find(params[:id])
     @design.destroy
-    redirect_to portfolio_designs_path(@design.portfolio)
+    # redirect_to portfolio_designs_path(@design.portfolio)
+    respond_to do |format|
+      format.html { redirect_to portfolio_designs_path(@design.portfolio_id) }
+      format.text { render partial: 'designs/list', locals: { design: @design }, formats: [:html] }
+    end
   end
 
   private
