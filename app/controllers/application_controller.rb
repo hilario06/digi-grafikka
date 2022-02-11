86819837a1b6f018ddb8c811863acb07b2620100
Gabriel <gabs1995@hotmail.com>
2022-02-10 21:44:50 -0500

    Styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
     # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
     # devise_parameter_sanitizer.for(:account_update) do |u| u.permit(:name, :last_name, :address, :email, :password, :current_password)
     devise_parameter_sanitizer.permit(:account_update,
-                                      keys: %i[name last_name address email password current_password])
+                                      keys: %i[name last_name address email password phone current_password])
   end
 end
